ae50587d51ab97cc8dca5790a739dd5d20d21f19
Carlos Carrero <carlos.carrero@yuxiglobal.com>
2019-01-21 20:12:49 -0500

    Adding test script

---
+++ TestScripts/Blogs.sql
@@ -0,0 +1,11 @@
+SELECT * FROM Blogs blog WHERE blog.Id = 9
+
+SELECT * FROM Posts post WHERE post.BlogId = 9
+
+SELECT		comm.* 
+FROM		Comments comm
+
+INNER JOIN	Posts post
+ON			comm.PostId = post.Id
+
+WHERE		post.BlogId = 9
