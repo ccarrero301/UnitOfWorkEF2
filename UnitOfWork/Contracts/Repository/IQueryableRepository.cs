e833853cd92f5f9159a09fe065d4511f9b30e2a4
Carlos Carrero <carlos.carrero@yuxiglobal.com>
2019-01-24 22:28:27 -0500

    Returning IQueryable repository interface to first implementation

--- UnitOfWork/Contracts/Repository/IQueryableRepository.cs
+++ UnitOfWork/Contracts/Repository/IQueryableRepository.cs
@@ -5,46 +5,52 @@
     using System.Linq.Expressions;
     using System.Threading;
     using System.Threading.Tasks;
-    using Patterns.Specification.Contracts;
+    using Microsoft.EntityFrameworkCore.Query;
     using PagedList;
 
     public interface IQueryableRepository<TEntity>
     {
-        IPagedList<TEntity> GetPagedList(ISpecification<TEntity> predicate = null,
+        IPagedList<TEntity> GetPagedList(Expression<Func<TEntity, bool>> predicate = null,
             Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
             int pageIndex = 0,
             int pageSize = 20,
             bool disableTracking = true);
 
-        Task<IPagedList<TEntity>> GetPagedListAsync(ISpecification<TEntity> predicate = null,
+        Task<IPagedList<TEntity>> GetPagedListAsync(Expression<Func<TEntity, bool>> predicate = null,
             Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
             int pageIndex = 0,
             int pageSize = 20,
             bool disableTracking = true,
             CancellationToken cancellationToken = default(CancellationToken));
 
         IPagedList<TResult> GetPagedList<TResult>(Expression<Func<TEntity, TResult>> selector,
-            ISpecification<TEntity> predicate = null,
+            Expression<Func<TEntity, bool>> predicate = null,
             Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
             int pageIndex = 0,
             int pageSize = 20,
             bool disableTracking = true) where TResult : class;
 
         Task<IPagedList<TResult>> GetPagedListAsync<TResult>(Expression<Func<TEntity, TResult>> selector,
-            ISpecification<TEntity> predicate = null,
+            Expression<Func<TEntity, bool>> predicate = null,
             Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
             int pageIndex = 0,
             int pageSize = 20,
             bool disableTracking = true,
             CancellationToken cancellationToken = default(CancellationToken)) where TResult : class;
 
-        Task<TEntity> GetFirstOrDefaultAsync(ISpecification<TEntity> predicate = null,
+        Task<TEntity> GetFirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate = null,
             Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
             bool disableTracking = true);
 
         Task<TResult> GetFirstOrDefaultAsync<TResult>(Expression<Func<TEntity, TResult>> selector,
-            ISpecification<TEntity> predicate = null,
+            Expression<Func<TEntity, bool>> predicate = null,
             Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null,
             bool disableTracking = true);
 
         IQueryable<TEntity> FromSql(string sql, params object[] parameters);
